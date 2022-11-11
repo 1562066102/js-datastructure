a55aea27ec07ff6913b313063837a3397427f237
1562066102 <1562066102@qq.com>
2022-11-11 14:13:48 +0800

    feat: ✨ 新增工具方法
    1. 新增defaultToString方法
    2. 新增ValuePair类
    3. defaultEquals方法迁移路径

--- src/utils/default-equals.ts
+++
@@ -1,4 +0,0 @@
-/** 两个元素默认的相等性比较函数 */
-const defaultEquals = <T>(a: T, b: T) => a === b;
-
-export default defaultEquals;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,17 @@
 export * from './linked-list-node';
 
-export {default as defaultEquals} from './default-equals';
+export {default as ValuePair} from './value-pair';
+
+/** 默认元素相等性比较函数 */
+export const defaultEquals = <T>(a: T, b: T) => a === b;
+
+/** 默认转换字符串函数 */
+export const defaultToString = (value: unknown) => {
+  if (value === null) return 'NULL';
+
+  if (value === undefined) return 'UNDEFINED';
+
+  if (typeof value === 'string' || value instanceof String) return `${value}`;
+
+  return value.toString();
+};
